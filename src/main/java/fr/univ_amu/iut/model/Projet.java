4b8072ab74be772ecd672b3ec4025ef87bec013f
Romain <rc2301@orange.fr>
2022-05-31 15:56:18 +0200

    Test new model

---
+++ src/main/java/fr/univ_amu/iut/model/Projet.java
@@ -0,0 +1,16 @@
+package fr.univ_amu.iut.model;
+
+public class Projet {
+
+    private int numero;
+    private Thematique thematique;
+    private Discipline discipline;
+    private Niveau niveau;
+    private Academie academie;
+    private Acteur acteur;
+    private Ressource ressource;
+    private String commentaire;
+
+    public
+
+}
