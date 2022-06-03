88b96996f93089bb5bc960ecb95a446d4100054c
Romain <rc2301@orange.fr>
2022-06-03 23:00:41 +0200

    Ajout de l'onglet Académie

--- src/main/java/fr/univ_amu/iut/contenu/usages/UsagesContenuControl.java
+++ src/main/java/fr/univ_amu/iut/contenu/usages/UsagesContenuControl.java
@@ -1,51 +1,50 @@
-package fr.univ_amu.iut.contenu.usages;
-
-import fr.univ_amu.iut.components.ListTemplateControl;
-import fr.univ_amu.iut.model.Thematique;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Control;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.FlowPane;
-
-import java.io.IOException;
-
-public class UsagesContenuControl extends FlowPane {
-
-    private Tab parentTab;
-
-    EventHandler<ActionEvent> ecouteur = new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            //Code de l'écouteur d'événement
-            TabPane tabPane = parentTab.getTabPane();
-            tabPane.getTabs().add(new Tab(((Control)event.getSource()).getId(),new ListTemplateControl(new String[]{"aa"})));
-        }
-    };
-
-    public UsagesContenuControl(Tab parentTab){
-        this.parentTab = parentTab;
-        initButtons();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UsagesContenuView.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    public void initButtons(){
-        for (Thematique thematique : Thematique.listeThematiques) {
-            Button bt = new Button(thematique.getNom());
-            bt.setPrefSize(120, 60);
-            bt.setId(thematique.getNom());
-            bt.setOnAction(ecouteur);
-            getChildren().add(bt);
-        }
-    }
-}
+//package fr.univ_amu.iut.contenu.usages;
+//
+//import fr.univ_amu.iut.model.Thematique;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Control;
+//import javafx.scene.control.Tab;
+//import javafx.scene.control.TabPane;
+//import javafx.scene.layout.FlowPane;
+//
+//import java.io.IOException;
+//
+//public class UsagesContenuControl extends FlowPane {
+//
+//    private Tab parentTab;
+//
+//    EventHandler<ActionEvent> ecouteur = new EventHandler<ActionEvent>() {
+//        @Override
+//        public void handle(ActionEvent event) {
+//            //Code de l'écouteur d'événement
+//            TabPane tabPane = parentTab.getTabPane();
+//            tabPane.getTabs().add(new Tab(((Control)event.getSource()).getId(),new ListTemplateControl(new String[]{"aa"})));
+//        }
+//    };
+//
+//    public UsagesContenuControl(Tab parentTab){
+//        this.parentTab = parentTab;
+//        initButtons();
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("UsagesContenuView.fxml"));
+//        fxmlLoader.setRoot(this);
+//        fxmlLoader.setController(this);
+//        try {
+//            fxmlLoader.load();
+//        } catch (IOException exception) {
+//            throw new RuntimeException(exception);
+//        }
+//    }
+//
+//    public void initButtons(){
+//        for (Thematique thematique : Thematique.listeThematiques) {
+//            Button bt = new Button(thematique.getNom());
+//            bt.setPrefSize(120, 60);
+//            bt.setId(thematique.getNom());
+//            bt.setOnAction(ecouteur);
+//            getChildren().add(bt);
+//        }
+//    }
+//}
